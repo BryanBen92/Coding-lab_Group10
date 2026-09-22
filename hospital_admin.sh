5f38881dc9324c58b58524aab9c2e76d537cdbed
jirellah-1 <s.ishimwe2@alustudent.com>
2026-09-22 04:55:08 -0700

    Add active and archived log checks

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+#!/bin/bash
+
+initialize_system() {
+    if [ -d "active_logs" ]; then
+        echo "active_logs already exists."
+    else
+        echo "Creating active_logs..."
+        mkdir active_logs
+    fi
+
+    if [ -d "archived_logs" ]; then
+        echo "archived_logs already exists."
+    else
+        echo "Creating archived_logs..."
+        mkdir archived_logs
+    fi
+}
+
